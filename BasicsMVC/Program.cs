4fa6fb43e8eb6c44359a4d85daf2c6340a87121a
erch47 <erik.chr1@hotmail.se>
2022-11-07 15:18:06 +0100

    Add project files.

---
+++ BasicsMVC/Program.cs
@@ -0,0 +1,37 @@
+namespace BasicsMVC
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            //Add services to the container
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            //Configure the http request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want tp change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+            
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=index}/{id?}");
+           
+            app.Run();
+        }
+
+    }
+}
